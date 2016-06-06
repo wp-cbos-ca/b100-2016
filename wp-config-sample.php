4d8a879a91fff77f853f62ea66475f01015226d2
Clarence Bos <wp@cbos.ca>
2016-06-06 18:17:09 -0400

    Updated.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,35 +1,50 @@
 <?php
 /**
- * Edit directly to avoid the web based interface.
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
  */
- 
-/** 
-* MySQL settings: obtain when creating a new database or from web host.
-*/
+
+// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
- * Generate from: 
- * 
- * https://api.wordpress.org/secret-key/1.1/salt/ 
- * 
- * Change to force new user logins.
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
  */
 define('AUTH_KEY',         'put your unique phrase here');
 define('SECURE_AUTH_KEY',  'put your unique phrase here');
@@ -43,44 +58,26 @@ define('NONCE_SALT',       'put your unique phrase here');
 /**#@-*/
 
 /**
- * Make random for better security or multi-site
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'tpt_'; 
+$table_prefix  = 'wp_';
 
 /**
-* Set to false for site development.
-*/
-define( 'WP_AUTO_UPDATE_CORE', true ); 
-
-/**
- * Set to true for site development.
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false ); 
-
-/**
-* Set to theme used for more reliable fallback.
-*/
-define( 'WP_DEFAULT_THEME', 'the-plugin-theme' );
-
-/**
-* Set to true to improve security.
-*/
-define( 'DISALLOW_UNFILTERED_HTML', true ); 
-
-/**
-* Set to true to prevent admin based file edits.
-*/
-define( 'DISALLOW_FILE_EDIT', true );
-
-/**
-* Set to true to speed up site. Requires server side cron.
-*/
-define( 'DISABLE_WP_CRON', false );
-
-/**
-* Defines WP Bundle Package used.
-*/
-define('WP_BUNDLE', 'WP43B100LTS' );
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
